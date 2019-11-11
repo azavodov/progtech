6cc583bfb6d558dc8fc79d9cc17ec4fa77835469
Andrey Zavodov <a.p.zavodov@gmail.com>
2019-11-11 16:08:22 +0300

    Implement endpoints for access to database

--- lab5/main/urls.py
+++ lab5/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, ),
+    path('get/names/<max_id>', views.get_names),
+    path('get/photo/<max_id>', views. get_photo),
 ]
--- lab5/main/views.py
+++ lab5/main/views.py
@@ -1,5 +1,24 @@
+import random
+
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from .models import Photo, Name
 
 
 def index(request):
     return render(request, 'index.html', locals())
+
+
+def get_names(request, max_id):
+    return JsonResponse({
+        'names': [
+            name.as_dict() for name in Name.objects.all() if name.pk <= int(max_id)
+        ]
+    })
+
+
+def get_photo(request, max_id):
+    return JsonResponse(random.choice(
+       [photo for photo in Photo.objects.all() if photo.pk <= int(max_id)]
+    ).as_dict())
