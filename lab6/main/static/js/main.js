01693cbc48e0dca29535f228dac45a0240dfcfe0
Andrey Zavodov <a.p.zavodov@gmail.com>
2019-11-26 18:34:46 +0300

    Add loader for buttons

--- lab6/main/static/js/main.js
+++ lab6/main/static/js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     get_channels()
         .then(() => {
-            load_feeds()
+            reload_feeds();
         });
 });
 
 let OFFSET = 0;
-let COUNT = 10;
+let COUNT = 5;
 
 function get_channels() {
     return new Promise((resolve, reject) => {
@@ -19,39 +19,55 @@ function get_channels() {
                         .text(channels[channel]['name']));
             });
             $('#channel').dropdown({
-                onChange: () => {
-                    OFFSET = 0;
-                    document.getElementById("entries_container").innerHTML = "";
-                    load_feeds();
-                }
+                onChange: () => { reload_feeds(); }
             });
             resolve();
         })
     });
 }
 
-function load_feeds(){
-    document.getElementById('feeds').classList.add("loading");
-    let channel_id = $('#channel').dropdown('get value');
-    axios.get(`/feeds?c_id=${channel_id}&offset=${OFFSET}&count=${COUNT}`).then(response => {
-        document.getElementById('feeds').classList.remove("loading");
-        let container = document.getElementById('entries_container');
-        let feeds = response.data['feeds'];
-        for (let i in feeds) {
-            container.innerHTML += `
-                <div class="item"">
-                    <div class="content">
-                      <a class="header" href="${feeds[i]['link']}" target="_blank">${feeds[i]['title']}</a>
-                      <div class="meta">
-                        <span class="cinema">${feeds[i]['published_time']}</span>
-                      </div>
-                      <div class="description">
-                        ${feeds[i]['description']}
-                      </div>
+function reload_feeds() {
+    OFFSET = 0;
+
+    let container = document.getElementById('reload_button');
+    container.classList.add("loading");
+
+    document.getElementById("entries_container").innerHTML = "";
+    load_feeds().finally(() => {
+        container.classList.remove("loading");
+    });
+}
+
+function load_feeds() {
+    return new Promise((resolve, reject) => {
+        let load_button = document.getElementById('load_button');
+        load_button.classList.add('loading');
+
+        let channel_id = $('#channel').dropdown('get value');
+        axios.get(`/feeds?c_id=${channel_id}&offset=${OFFSET}&count=${COUNT}`).then(response => {
+            load_button.classList.remove('loading');
+            let container = document.getElementById('entries_container');
+
+            let feeds = response.data['feeds'];
+            for (let i in feeds) {
+                container.innerHTML += `
+                    <div class="item"">
+                        <div class="content">
+                          <a class="header" href="${feeds[i]['link']}" target="_blank">${feeds[i]['title']}</a>
+                          <div class="meta">
+                            <span class="cinema">${feeds[i]['published_time']}</span>
+                          </div>
+                          <div class="description">
+                            ${feeds[i]['description']}
+                          </div>
+                        </div>
                     </div>
-                </div>
-            `;
-        }
-        OFFSET += COUNT;
-    })
+                `;
+            }
+
+            OFFSET += COUNT;
+            resolve();
+        })
+
+    });
 }
