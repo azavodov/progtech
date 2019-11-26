ba5a4f4e3d057ddd5d735ffefce21fbc6cd95951
Andrey Zavodov <a.p.zavodov@gmail.com>
2019-11-26 15:16:57 +0300

    Add models for rss reader

--- lab6/main/models.py
+++ lab6/main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    url = models.URLField(max_length=1024)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.url
+
+    def as_dict(self):
+        return {
+            "id": self.pk,
+            "url": self.url,
+            "name": self.name,
+        }
+
+
+class Feed(models.Model):
+    channel = models.ForeignKey(Channel, blank=False, null=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=2000, blank=True, null=True)
+    link = models.CharField(max_length=2000, unique=True)
+    description = models.TextField(blank=True, null=True)
+    published_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def as_dict(self):
+        return {
+            "id": self.pk,
+            "source_id": self.channel.pk,
+            "title": self.title,
+            "link": self.link,
+            "description": self.description,
+            "published_time": self.published_time,
+        }
